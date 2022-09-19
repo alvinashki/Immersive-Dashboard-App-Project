5406470c9d3c418ee2a64c2881006372216c3670
Alvinashki <alvinikhsani14@gmail.com>
2022-09-19 22:56:36 +0700

    login feature

---
+++ features/login/delivery/hadler.go
@@ -0,0 +1,41 @@
+package delivery
+
+import (
+	"gp3/features/login"
+	"gp3/utils/helper"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Delivery struct {
+	authUsecase login.UsecaseInterface
+}
+
+func New(e *echo.Echo, usecase login.UsecaseInterface) {
+
+	handler := Delivery{
+		authUsecase: usecase,
+	}
+
+	e.POST("/auth", handler.Auth)
+
+}
+
+func (delivery *Delivery) Auth(c echo.Context) error {
+
+	var req Request
+	errBind := c.Bind(&req)
+	if errBind != nil {
+		return c.JSON(400, helper.FailedResponseHelper("wrong request"))
+	}
+
+	str := delivery.authUsecase.LoginAuthorized(req.Email, req.Password)
+	if str == "please input email and password" || str == "email not found" || str == "wrong password" {
+		return c.JSON(400, helper.FailedResponseHelper(str))
+	} else if str == "failed to created token" {
+		return c.JSON(500, helper.FailedResponseHelper(str))
+	} else {
+		return c.JSON(200, helper.SuccessDataResponseHelper("Login Success", str))
+	}
+
+}
---
+++ features/login/usecase/logic.go
@@ -0,0 +1,37 @@
+package usecase
+
+import (
+	"gp3/features/login"
+	"gp3/middlewares"
+)
+
+type authUsecase struct {
+	authData login.DataInterface
+}
+
+func New(data login.DataInterface) login.UsecaseInterface {
+	return &authUsecase{
+		authData: data,
+	}
+}
+
+func (usecase *authUsecase) LoginAuthorized(email, password string) string {
+
+	if email == "" || password == "" {
+		return "please input email and password"
+	}
+
+	results, errEmail := usecase.authData.LoginUser(email)
+	if errEmail != nil {
+		return "email not found"
+	}
+
+	token, errToken := middlewares.CreateToken(int(results.ID))
+
+	if errToken != nil {
+		return "error to created token"
+	}
+
+	return token
+
+}
