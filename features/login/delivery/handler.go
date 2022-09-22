0dfaed743c7a6ad752de2afe4bd0d78a85d906df
muhdwiar <muhdwi45@gmail.com>
2022-09-22 12:04:24 +0700

    modify login feature

--- features/login/delivery/handler.go
+++ features/login/delivery/handler.go
@@ -30,13 +30,13 @@ func (delivery *Delivery) Auth(c echo.Context) error {
 		return c.JSON(400, helper.FailedResponseHelper("wrong request"))
 	}
 
-	str := delivery.authUsecase.LoginAuthorized(req.Email, req.Password)
+	str, role := delivery.authUsecase.LoginAuthorized(req.Email, req.Password)
 	if str == "please input email and password" || str == "email not found" || str == "wrong password" {
 		return c.JSON(400, helper.FailedResponseHelper(str))
 	} else if str == "failed to created token" {
 		return c.JSON(500, helper.FailedResponseHelper(str))
 	} else {
-		return c.JSON(200, helper.SuccessDataResponseHelper("Login Success", str))
+		return c.JSON(200, helper.SuccessDataResponseHelper("Login Success", FromCore(str, role)))
 	}
 
 }
