1840f416781248d0d967625f3d860505cfff47e0
muhdwiar <muhdwi45@gmail.com>
2022-09-22 08:40:26 +0700

    add create-class feture

--- features/class/data/query.go
+++ features/class/data/query.go
@@ -16,3 +16,14 @@ func New(db *gorm.DB) class.DataInterface {
 	}
 
 }
+
+func (repo *classData) CreateClass(dataClass class.Core) (int, error) {
+	newClass := fromCore(dataClass)
+
+	tx := repo.db.Create(&newClass)
+	if tx.Error != nil {
+		return 0, tx.Error
+	}
+
+	return int(tx.RowsAffected), nil
+}
--- features/class/delivery/handler.go
+++ features/class/delivery/handler.go
@@ -2,6 +2,8 @@ package delivery
 
 import (
 	"gp3/features/class"
+	"gp3/utils/helper"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,8 +13,27 @@ type CLassDelivery struct {
 }
 
 func New(e *echo.Echo, usecase class.UsecaseInterface) {
-	// handler := &CLassDelivery{
-	// 	classUsecase: usecase,
-	// }
+	handler := &CLassDelivery{
+		classUsecase: usecase,
+	}
 
+	e.POST("/class", handler.PostNewClass)
+
+}
+
+func (delivery *CLassDelivery) PostNewClass(c echo.Context) error {
+	var classRequestData ClassRequest
+
+	errBind := c.Bind(&classRequestData)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper("fail bind data"))
+	}
+
+	row, err := delivery.classUsecase.InsertClass(ToCore(classRequestData))
+
+	if err != nil || row != 1 {
+		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper(err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponseHelper("success add new class"))
 }
